9bfc15ab1e9ccdc330548d23d636b28ad7ab3dcb
binthec <binthec@gmail.com>
2024-03-30 17:54:08 +0900

    propsで渡ってきたbookをリアクティブな値として再定義

--- resources/js/Components/DeleteModal.vue
+++ resources/js/Components/DeleteModal.vue
@@ -1,19 +1,21 @@
 <script setup>
 import {Modal} from "@kouts/vue-modal";
 import {Link} from "@inertiajs/vue3";
+import {computed} from "vue";
 
-defineProps(['modal']);
+const props = defineProps(['modal']);
+const book = computed(() => props.modal.book);
 </script>
 
 <template>
     <Modal v-model="modal.show" title="削除を実行します">
-        <p class="bg-warning p-2">【ID:{{ modal.book.id }}】{{ modal.book.filename }}</p>
+        <p class="bg-warning p-2">【ID:{{ book.id }}】{{ book.filename }}</p>
         <p>
             削除します。よろしいですか。
             <span class="text-danger font-semibold">※この処理は取り消せません。</span>
         </p>
         <div class="d-flex justify-content-center">
-            <img :src="'storage/' + modal.book.filepath" class="mh-500"/>
+            <img :src="'storage/' + book.filepath" class="mh-500"/>
         </div>
 
         <div class="mt-4">
