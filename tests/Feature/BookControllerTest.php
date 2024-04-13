765e37d62427cc3d019df9d9e60a57e6868e7899
binthec <binthec@gmail.com>
2024-04-13 22:51:20 +0900

    storeメソッドのテストを追加

--- tests/Feature/BookControllerTest.php
+++ tests/Feature/BookControllerTest.php
@@ -2,12 +2,11 @@
 
 namespace Tests\Feature;
 
-use App\Models\Book;
-use Faker\Factory;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 use Inertia\Testing\AssertableInertia as AssertInertia;
 
@@ -15,6 +14,11 @@ class BookControllerTest extends TestCase
 {
 
     use RefreshDatabase;
+
+    /**
+     * @var string
+     */
+    private string $fileDirPath;
 
     /**
      * @return void
@@ -25,10 +29,13 @@ class BookControllerTest extends TestCase
 
         // この feature テストで使う user を作成、保存
         $this->user = User::factory()->create();
+        // ファイルが保存されるディレクトリパス
+        $this->fileDirPath = 'books/' . $this->user->id . '/';
     }
 
     /**
-     * A basic feature test example.
+     * test index()
+     * @return void
      */
     public function testIndex(): void
     {
@@ -39,8 +46,36 @@ class BookControllerTest extends TestCase
         // ログイン済みの場合、books 一覧画面が表示される
         $response = $this->actingAs($this->user)->get('/books');
         $response->assertOk();
-        $response->assertInertia(fn (AssertInertia $page) => $page
+        $response->assertInertia(fn(AssertInertia $page) => $page
             ->has('books') // books というプロパティが渡されているか
         );
+    }
+
+    /**
+     * test store()
+     * @return void
+     */
+    public function testStore(): void
+    {
+        Storage::fake('public');
+
+        $file = UploadedFile::fake()->image('test.jpg');
+
+        $response = $this
+            ->actingAs($this->user)
+            ->post(route('books.store'), [
+                'file' => $file,
+                'filename' => 'filename_test',
+                'size' => 10,
+                'height' => 10,
+                'width' => 10,
+                'lastModified' => now(),
+            ]);
+
+        // ファイルが生成されたことを確認
+        Storage::disk('public')->assertExists($this->fileDirPath . $file->hashName());
+
+        // アップロード後、index にリダイレクトされることを確認
+        $response->assertRedirect(route('books.index'));
     }
 }
